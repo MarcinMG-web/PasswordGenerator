bef7b34088d9bdeb074a793f2b08819769d026c8
MarcinMG-web <gierada.marcin@gmail.com>
2021-06-25 14:25:59 +0200

    Added mock table with results

--- src/component/SavedPasswords.js
+++ src/component/SavedPasswords.js
@@ -7,7 +7,23 @@ const SavedPasswords = () => {
       <div className='mainContainerSavedPasswords-text'>Saved Passwords:</div>
 
       <div className='mainContainerSavedPasswords-table'>
-          Place for table
+        <table style={{ width: '100%' }}>
+          <tr>
+            <th>#</th>
+            <th>Password for:</th>
+            <th>Password</th>
+          </tr>
+          <tr>
+            <th>1</th>
+            <td>Jill</td>
+            <td>id2".h),tg*+l%$6ksue",n12voa</td>
+          </tr>
+          <tr>
+            <th>2</th>
+            <td>Smith</td>
+            <td>j6n'+b)8mwf7nsp&h92)65tv)i6/</td>
+          </tr>
+        </table>
       </div>
 
       <div className='mainContainerSavedPasswords-buttons'>
